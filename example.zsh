f4026668bd6fe60c28b10d664da76dc0d5a34c38
Hieu Nguyen <hieu.nguyen@Hieu-FEB22.home.ssosol.com>
2022-02-19 21:34:43 -0700

    first commit

---
+++ example.zsh
@@ -0,0 +1,16 @@
+# You can put files here to add functionality separated per file, which
+# will be ignored by git.
+# Files on the custom/ directory will be automatically loaded by the init
+# script, in alphabetical order.
+
+# For example: add yourself some shortcuts to projects you often work on.
+#
+# brainstormr=~/Projects/development/planetargon/brainstormr
+# cd $brainstormr
+#
+#export JAVA_HOM=/Library/Java/JavaVirtualMachines/openjdk8-zulu/Contents/Home
+#export JAVA_HOME=/Library/Java/JavaVirtualMachines/openjdk11-zulu/Contents/Home
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/openjdk17-zulu/Contents/Home
+
+
+source /opt/local/share/nvm/init-nvm.sh
