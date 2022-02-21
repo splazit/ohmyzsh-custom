fa4568c4e7fb7f4602978768fdc88af16e173937
Hieu Nguyen <hieu.nguyen@Hieu-FEB22.home.ssosol.com>
2022-02-20 21:40:00 -0700

    add zsh exa alias

--- example.zsh
+++ example.zsh
@@ -11,6 +11,7 @@
 #export JAVA_HOM=/Library/Java/JavaVirtualMachines/openjdk8-zulu/Contents/Home
 #export JAVA_HOME=/Library/Java/JavaVirtualMachines/openjdk11-zulu/Contents/Home
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/openjdk17-zulu/Contents/Home
-
-
+export EDITOR=nvim
 source /opt/local/share/nvm/init-nvm.sh
+export GIT_AUTO_FETCH_INTERVAL=1200
+alias e='nvim'
